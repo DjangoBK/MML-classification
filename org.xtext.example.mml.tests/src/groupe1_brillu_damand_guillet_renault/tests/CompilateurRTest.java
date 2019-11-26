cefce06547232905c1fa40863f2081209807fbcb
Renault Pierrick <pierrick.renault@etudiant.univ-rennes1.fr>
2019-11-26 16:05:30 +0100

    update TestR

--- org.xtext.example.mml.tests/src/groupe1_brillu_damand_guillet_renault/tests/CompilateurRTest.java
+++ org.xtext.example.mml.tests/src/groupe1_brillu_damand_guillet_renault/tests/CompilateurRTest.java
@@ -1,5 +1,9 @@
 package groupe1_brillu_damand_guillet_renault.tests;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
 import org.eclipse.xtext.testing.util.ParseHelper;
@@ -8,8 +12,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.xtext.example.mydsl.mml.MMLModel;
 import org.xtext.example.mydsl.tests.MmlInjectorProvider;
 
+import com.google.common.io.Files;
 import com.google.inject.Inject;
 
+import groupe1_brillu_damand_guillet_renault.compilateur.Compilateur;
 import groupe1_brillu_damand_guillet_renault.compilateur.CompilateurR;
 
 @ExtendWith(InjectionExtension.class)
@@ -22,12 +28,31 @@ public class CompilateurRTest {
 	@Test
 	public void testR() throws Exception {
 		MMLModel result = parseHelper.parse("datainput \"C:/CSVFile/iris.csv\" separator ;\n"
-				+ "mlframework scikit-learn\n"
+				+ "mlframework R\n"
 				+ "algorithm DT\n"
 				+ "TrainingTest { percentageTraining 70 }\n"
 				+ "recall\n"
 				+ "");
-		CompilateurR c = new CompilateurR(result);
-		c.traitement(result);
+		
+		//Compilateur c = Compilateur.(result);
+		//c.traitement(result);
+		
+		String pandasCode = Compilateur.loadData(result);
+		pandasCode += Compilateur.traitementAlgo(result);	
+		
+		Files.write(pandasCode.getBytes(), new File("mml.py"));
+		// end of Python generation
+		
+		
+		/*
+		 * Calling generated Python script (basic solution through systems call)
+		 * we assume that "python" is in the path
+		 */
+		Process p = Runtime.getRuntime().exec("python mml.py");
+		BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line; 
+		while ((line = in.readLine()) != null) {
+			System.out.println(line);
+	    }
 	}
 }
