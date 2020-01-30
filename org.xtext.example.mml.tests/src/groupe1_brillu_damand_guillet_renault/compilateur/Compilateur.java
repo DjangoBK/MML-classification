9d2660a27b04e2a74750811b47a3d86f7ef7f916
A730437 <A730437@172.23.143.183>
2020-01-30 15:49:19 +0100

    Suppression methode inutile Compilateur

--- org.xtext.example.mml.tests/src/groupe1_brillu_damand_guillet_renault/compilateur/Compilateur.java
+++ org.xtext.example.mml.tests/src/groupe1_brillu_damand_guillet_renault/compilateur/Compilateur.java
@@ -25,55 +25,16 @@ public class Compilateur {
 	@Inject
 	static ParseHelper<MMLModel> parseHelper;
 	
-	static String DEFAULT_COLUMN_SEPARATOR = ","; // by default
+	static String DEFAULT_COLUMN_SEPARATOR = ","; // by default	
 	
-	public static void createFile() throws Exception {
-		MMLModel result = parseHelper.parse("datainput \"C:/CSVFile/iris.csv\" separator ;\n"
-				+ "mlframework scikit-learn\n"
-				+ "algorithm DT\n"
-				+ "TrainingTest { percentageTraining 70 }\n"
-				+ "recall\n"
-				+ "");
-		
-		String pandasCode = loadData(result);
-		pandasCode += traitementAlgo(result);
-		
-		Files.write(pandasCode.getBytes(), new File("mml_from_compiler.py"));
-		// end of Python generation
-		
-		
-		/*
-		 * Calling generated Python script (basic solution through systems call)
-		 * we assume that "python" is in the path
-		 */
-		Process p = Runtime.getRuntime().exec("python mml.py");
-		BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		String line; 
-		while ((line = in.readLine()) != null) {
-			System.out.println(line);
-	    }
-	}
-	
-	
-	public static String loadData(MMLModel result) {
-		DataInput dataInput = result.getInput();
-		String fileLocation = dataInput.getFilelocation();
-		
-		String pythonImport = "import pandas as pd\n"; 
-		String csv_separator = DEFAULT_COLUMN_SEPARATOR;
-		CSVParsingConfiguration parsingInstruction = dataInput.getParsingInstruction();
-		
-		if (parsingInstruction != null) {
-			csv_separator = parsingInstruction.getSep().toString();
-		}
-		return pythonImport + "\n";
-	}
+	String algo;
+	String framework;
 	
 	public static String traitementAlgo(MMLModel result) {
-		//FrameworkLang framworkLang = result.getAlgorithm().getFramework();
 		EList<MLChoiceAlgorithm> algorithms = result.getAlgorithms();
 		String res = "";
 		System.err.println("traitement algo");
+		
 		for(MLChoiceAlgorithm mlcalgo : algorithms) {
 			FrameworkLang framworkLang = mlcalgo.getFramework();
 			if(framworkLang == FrameworkLang.SCIKIT) {
@@ -97,8 +58,6 @@ public class Compilateur {
 		return "'" + val + "'";
 	}
 	
-	public static void main (String[] args) throws Exception{
-		
-	}
+	
 
 }
