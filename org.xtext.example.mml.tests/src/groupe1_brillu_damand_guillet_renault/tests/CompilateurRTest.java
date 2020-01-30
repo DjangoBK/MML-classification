16cc034f7a15d77b357528c5638e50458b03ed43
A730437 <A730437@172.23.143.183>
2020-01-30 15:33:12 +0100

    Compilateur R remove useless import + debut gestion des arguments pour le csv

--- org.xtext.example.mml.tests/src/groupe1_brillu_damand_guillet_renault/tests/CompilateurRTest.java
+++ org.xtext.example.mml.tests/src/groupe1_brillu_damand_guillet_renault/tests/CompilateurRTest.java
@@ -4,25 +4,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 
-import javax.xml.crypto.AlgorithmMethod;
-
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
 import org.eclipse.xtext.testing.util.ParseHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.xtext.example.mydsl.mml.MLChoiceAlgorithm;
 import org.xtext.example.mydsl.mml.MMLModel;
-import org.xtext.example.mydsl.services.MmlGrammarAccess.MLChoiceAlgorithmElements;
 import org.xtext.example.mydsl.tests.MmlInjectorProvider;
 
 import com.google.common.io.Files;
 import com.google.inject.Inject;
 
 import groupe1_brillu_damand_guillet_renault.compilateur.Compilateur;
-import groupe1_brillu_damand_guillet_renault.compilateur.CompilateurR;
-import groupe1_brillu_damand_guillet_renault.compilateur.CompilateurRPierreWork;
-
 @ExtendWith(InjectionExtension.class)
 @InjectWith(MmlInjectorProvider.class)
 public class CompilateurRTest {
@@ -30,7 +23,8 @@ public class CompilateurRTest {
 	@Inject
 	ParseHelper<MMLModel> parseHelper;
 	
-	final String CSV_FOLDER = "\"C:/Users/pbril/Documents/R_workspace/iris.csv\"";
+	//final String CSV_FOLDER = "\"C:/Users/pbril/Documents/R_workspace/iris.csv\"";
+	final String CSV_FOLDER = "\"C:/Users/A730437/Documents/MIAGE/IDM/R_IDM/iris.csv\"";
 	
 	@Test
 	public void testR() throws Exception {
@@ -51,11 +45,16 @@ public class CompilateurRTest {
 		long startTime = System.nanoTime();
 		Process p = Runtime.getRuntime().exec("Rscript mml.R");
 		BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		String line; 
+		String line;
+		String last="";
+		int c = 0;
 		while ((line = in.readLine()) != null) {
-			System.out.println(line);
+			//System.out.println(line);
+			last = line;
 	    }
 		long elapsedTime = System.nanoTime() - startTime;
+		double metric = Double.parseDouble(last);
+		System.err.println(" ; " + metric);
 		System.err.println("temps d'execution : " + elapsedTime/1000000000.0);
 		in.close();
 	}
