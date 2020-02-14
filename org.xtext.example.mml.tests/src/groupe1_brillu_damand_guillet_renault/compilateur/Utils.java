42bdbe864848e4f804d2a3248f2e1b331204c88e
Renault Pierrick <pierrick.renault@etudiant.univ-rennes1.fr>
2020-02-14 09:53:50 +0100

    ajout fonction pour ecrire sur un csv pour présentation

--- org.xtext.example.mml.tests/src/groupe1_brillu_damand_guillet_renault/compilateur/Utils.java
+++ org.xtext.example.mml.tests/src/groupe1_brillu_damand_guillet_renault/compilateur/Utils.java
@@ -1,5 +1,23 @@
 package groupe1_brillu_damand_guillet_renault.compilateur;
 
+import java.io.FileWriter;
+
 public class Utils {
 
+	/**
+     * Permet d'ecrire dans un csv
+     * @param data - la chaine que l'on veut ecrire
+     */
+    public static void createCsvFile(String data) {
+
+        try {
+            //penser a changer le chemin du file
+            FileWriter writer = new FileWriter("./src/main/resources/data.csv",true);
+            writer.write(data+"\n");
+            writer.close();
+
+        } catch (Exception e) {
+            System.out.println("update impossible");
+        }
+    }
 }
